4adb8368ffcaefdb909522562efef035d178c13c
Raphael Torquato <89878688+raphaeltorquat0@users.noreply.github.com>
2022-06-03 12:34:33 -0300

    Initial Commit

---
+++ swiftui_redux/swiftui_reduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_reduxApp.swift
+//  swiftui_redux
+//
+//  Created by Raphael Torquato on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_reduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
